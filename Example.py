cbd7d7896816b42c3d0634d1df1b6c3b34d691b2
Mathilda Campillo <108595835+Mathilda27@users.noreply.github.com>
2023-10-24 17:04:20 -0500

    Example.py

---
+++ Example.py
@@ -0,0 +1,8 @@
+# Polygon Coordinates
+poly=np.array([[ 7.98110695,  8.93841337],       
+       [-7.98546945,  4.60776173],
+       [-7.76624386, -9.52465779],
+       [-3.43910333, -9.39119483]])
+
+# Fair Partitions Method Command
+Equipartition.partition(poly,6,Regions=False,Areas=True,Perimeters=True)
