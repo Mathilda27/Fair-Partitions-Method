94a28ca58e169ce34eca0d7e947524711b24589b
Mathilda Campillo <108595835+Mathilda27@users.noreply.github.com>
2023-10-24 17:05:30 -0500

    Fair_Partitions_Method.py

--- class_Equipartition.py
+++ class_Equipartition.py
@@ -1,17 +1,3 @@
-from dbm.dumb import error
-import scipy as sp
-import numpy as np
-import matplotlib.pyplot as plt
-import copy
-import time, timeit
-from scipy.spatial import Voronoi, voronoi_plot_2d
-import pandas as pd
-from scipy.spatial import ConvexHull, convex_hull_plot_2d
-from matplotlib.path import Path
-from matplotlib import cm
-from numpy.ma.core import sqrt
-from matplotlib.colors import ListedColormap, LinearSegmentedColormap
-
 class Equipartition:
 
     def __init__(self,polygon,number_of_regions):
